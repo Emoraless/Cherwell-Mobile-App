e7ed7f8d0b6475a004665d8afe90a9e9b6f11377
Eric Morales <Emorales@ou.edu>
2019-01-09 20:40:58 -0600

    Create QuickTicketExpress.php

---
+++ QuickTicketExpress.php
@@ -0,0 +1,31 @@
+<?php
+
+	header('Content-Type: application/json');
+	require "..\Includes\QuickTicketApi.php";
+	require "..\Includes\Connection.php";
+
+	//Verifies the OUNetId inserted into the program
+
+	$fourfour = $_GET['four'];
+
+	$conn = new Connection('DB-NAME');
+	$quick = new QuickTicketApi($conn);
+
+	if((time() - $conn->getTimeTest()) > 1170){
+		$token = $quick->requestToken();
+		$conn->upTokenTest($token,time());
+	}
+	else{
+		$token = $conn->tokenGrab(30);
+	}
+
+	$returnFour = $quick->getFullNameFromFourByFour($fourfour,$token);
+
+	if($returnFour == "Guest User"){
+		echo "FALSE";
+	}
+	else{
+		echo "TRUE";
+	}
+
+?>
